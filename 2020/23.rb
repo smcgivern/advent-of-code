5fea3a5090b5d015f3222fb9a27807e8558f986e
Sean McGivern <sean@mcgivern.me.uk>
2020-12-27 16:22:58 +0000

    Add day 23 part 2 solution

--- 2020/23.rb
+++ 2020/23.rb
@@ -1,17 +1,58 @@
-def move(cups)
-  pick_up = cups.slice!(1, 3)
-  destination = cups.sort.reverse.slice_after(cups.first).to_a.last.first
+def move(cups, max, current)
+  cup_1 = cups[current]
+  cup_2 = cups[cup_1]
+  cup_3 = cups[cup_2]
+  succ = cups[cup_3]
 
-  cups.rotate!
-  cups.insert(cups.find_index(destination).succ, *pick_up)
+  destination = current.pred
+
+  while [0, cup_1, cup_2, cup_3].include?(destination)
+    if destination <= 0
+      destination = max
+    else
+      destination -= 1
+    end
+  end
+
+  cups[current] = succ
+  cups[cup_3] = cups[destination]
+  cups[destination] = cup_1
+
+  succ
+end
+
+def play(seed, size, steps)
+  init = seed.split('').map(&:to_i)
+  cups = [nil] +
+    init.zip(init.rotate).sort_by(&:first).map(&:last)[0..-2] +
+    seed.length.succ.upto(size).to_a +
+    [init.first]
+  current = init.first
+
+  steps.times { current = move(cups, size, current) }
+
+  cups
 end
 
 def part1
-  cups = '712643589'.split('').map(&:to_i)
+  result = play('712643589', 9, 100)
+  current = 1
+  output = ''
 
-  100.times { move(cups) }
+  loop do
+    current = result[current]
 
-  cups.slice_after(1).to_a.reverse.flatten.join[0..-2]
+    return output if current == 1
+
+    output += current.to_s
+  end
+end
+
+def part2
+  result = play('712643589', 1000000, 10000000)
+
+  result[1] * result[result[1]]
 end
 
 puts part1
+puts part2
